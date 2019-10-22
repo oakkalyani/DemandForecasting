b94fa2670443e7e1d04fa43f79ca679354e8fa6f
Kalyani <oakkalyani10@gmail.com>
2019-10-21 22:36:19 -0500

    CookieCutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='UCI ML Repository. Use of forecasting techniques to forecast demand ',
+    author='Kalyani ',
+    license='',
+)
